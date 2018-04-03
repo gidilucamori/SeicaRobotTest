d27d4ca826405b93aa1089abdfe6dfd2a93250dd
Luca Mori <mori.luca@hotmail.it>
2018-04-03 16:12:13 +0200

    test ricezione e scrittura dati tcp

---
+++ Console/Robot/Robot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seica
+{
+    public class Robot
+    {
+        private string _ipAddress { get; set; }
+
+        public Robot(string s)
+        {
+
+        }
+        public List<RobotPoint> Points { get; set; }
+
+
+    }
+
+    public class RobotPoint
+    {
+        public List<float> Point { get; set; }
+    }
+
+}
